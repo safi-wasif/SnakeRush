b70f108cef4cfce94bbf75c4291aecd12ba80432
safi-wasif <142847514+safi-wasif@users.noreply.github.com>
2024-02-03 18:17:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -114,6 +114,72 @@ function gameEngine(){
 
 
 //main code
+//android
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    
+    hiScore.innerHTML="Hi-Score: "+hi;
+    scoreBox.innerHTML="Score: "+score;
+    musicSound.play();
+    inputDir = {x:0 , y:1} //start game
+    moveSound.play();
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+        } else {
+            /* left swipe */
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+        } else { 
+            /* up swipe */
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+//windows
 window.requestAnimationFrame(main);
 window.addEventListener('keydown' , e=> {
     
